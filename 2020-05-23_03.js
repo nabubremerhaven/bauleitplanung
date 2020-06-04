f945f8e64f371fc823082234ef3ffe879c365f29
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-06-04 19:37:17 +0200

    Update and rename 2020-05-23_02.js to 2020-05-23_03.js

--- 2020-05-23_03.js
+++ 2020-05-23_03.js
@@ -31,7 +31,7 @@ bremerhaven001 = ["bremerhaven001", 'Bebauungsplan Nr. 494 "Green Economy-Gebiet
 
 loxstedt001 = ["loxstedt001", "derzeit keine Stellungnahmen","","",""]
 
-schiffdorf001 = ["schiffdorf001", "derzeit keine Stellungnahmen","","",""]
+schiffdorf001 = ["schiffdorf001", 'Spaden: Bebauungsplan Nr. 103 "Betreutes Wohnen am Schierholzweg"',"spaden-betreutes-wohnen-am-schierholzweg/","Öffentlichkeit","05/2020"]
 
 wnk002 = ["wnk002", 'Wremen: Teil-Flächennutzungsplan, 10. Änderung',"wremen-wohnanlage-marschenhof/","frühzeitig","05/2020"]
 wnk001 = ["wnk001", 'Wremen: Bebauungsplan Nr. 32 "Wohnanlage Marschenhof"',"wremen-wohnanlage-marschenhof/","frühzeitig","05/2020"]
