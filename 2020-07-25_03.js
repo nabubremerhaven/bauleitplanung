20c546974a0823c14b10c16e0382b0910505a4f8
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-07-25 18:09:22 +0200

    Update and rename 2020-07-25_02.js to 2020-07-25_03.js

--- 2020-07-25_03.js
+++ 2020-07-25_03.js
@@ -26,7 +26,7 @@ function over_geestland003() {
 geestland004 = ["geestland004", 'Neuenwalde: Bebauungsplan Nr. 226 "Schafdamm"', "neuenwalde-schafdamm/", "frühzeitig", "07/2020"]
 geestland003 = ["geestland003", 'Debstedt: Bebauungsplan Nr. 12 "Östlich Malerwinkel"', "debstedt-östlich-malerwinkel/", "frühzeitig", "07/2020"]
 geestland002 = ["geestland002", 'Bederkesa: Bebauungsplan Nr. 12 "Erholungszentrum", 12. Änderung', "geestland-teil-fnp-bederkesa-10-änderung/", "frühzeitig", "11/2019"]
-geestland001 = ["geestland001", "Debstedt: 'Rosenburg", 2. Änderung des Teil-Flächennutzungsplans', "geestland-teil-fnp-langen-2-änderung/", "Öffentlichkeit", "05/2019"]
+geestland001 = ["geestland001", 'Debstedt: "Rosenburg", 2. Änderung des Teil-Flächennutzungsplans', "geestland-teil-fnp-langen-2-änderung/", "Öffentlichkeit", "05/2019"]
 
 bremerhaven001 = ["bremerhaven001", 'Bebauungsplan Nr. 494 "Green Economy-Gebiet Lune Delta"',"bremerhaven-green-economy-gebiet-lune-delta/","frühzeitig","02/2020"]
 
