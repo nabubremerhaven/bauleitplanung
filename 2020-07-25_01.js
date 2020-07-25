add99e6b1bf5b0ef68ad9b88b80ff7f499964574
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-07-25 18:00:30 +0200

    Update and rename 2020-05-23_03.js to 2020-07-25_01.js

--- 2020-07-25_01.js
+++ 2020-07-25_01.js
@@ -23,21 +23,22 @@ function over_geestland003() {
         
 /* DEFINIEREN DER BPLAN-INFORMATIONEN */    
 
-geestland003 = ["geestland003", "Bederkesa: Teil-Flächennutzungsplan, 10. Änderung", "geestland-teil-fnp-bederkesa-10-änderung/", "frühzeitig", "11/2019"]
+geestland004 = ["geestland004", 'Neuenwalde: Bebauungsplan Nr. 226 "Schafdamm"', "neuenwalde-schafdamm/", "frühzeitig", "07/2020"]
+geestland003 = ["geestland003", 'Debstedt: Bebauungsplan Nr. 12 "Östlich Malerwinkel"', "debstedt-östlich-malerwinkel/", "frühzeitig", "07/2020"]
 geestland002 = ["geestland002", 'Bederkesa: Bebauungsplan Nr. 12 "Erholungszentrum", 12. Änderung', "geestland-teil-fnp-bederkesa-10-änderung/", "frühzeitig", "11/2019"]
-geestland001 = ["geestland001", "Langen: Teil-Flächennutzungsplan, 2. Änderung", "geestland-teil-fnp-langen-2-änderung/", "Öffentlichkeit", "05/2019"]
+geestland001 = ["geestland001", "Debstedt: 'Rosenburg", 2. Änderung des Teil-Flächennutzungsplans', "geestland-teil-fnp-langen-2-änderung/", "Öffentlichkeit", "05/2019"]
 
 bremerhaven001 = ["bremerhaven001", 'Bebauungsplan Nr. 494 "Green Economy-Gebiet Lune Delta"',"bremerhaven-green-economy-gebiet-lune-delta/","frühzeitig","02/2020"]
 
 loxstedt001 = ["loxstedt001", "derzeit keine Stellungnahmen","","",""]
 
+schiffdorf002 = ["schiffdorf992", 'Wehdel: Bebauungsplan Nr. 96 "Moorteilsweg"', "wehdel-moorteilsweg", "Öffentlichkeit", "07/2020"]
 schiffdorf001 = ["schiffdorf001", 'Spaden: Bebauungsplan Nr. 103 "Betreutes Wohnen am Schierholzweg"',"spaden-betreutes-wohnen-am-schierholzweg/","Öffentlichkeit","05/2020"]
 
-wnk002 = ["wnk002", 'Wremen: Teil-Flächennutzungsplan, 10. Änderung',"wremen-wohnanlage-marschenhof/","frühzeitig","05/2020"]
 wnk001 = ["wnk001", 'Wremen: Bebauungsplan Nr. 32 "Wohnanlage Marschenhof"',"wremen-wohnanlage-marschenhof/","frühzeitig","05/2020"]
 
-bplaene = [geestland001, geestland002, geestland003, bremerhaven001, loxstedt001,
-               schiffdorf001, wnk001, wnk002]   
+bplaene = [geestland001, geestland002, geestland003, geestland004, bremerhaven001, loxstedt001,
+               schiffdorf001, schiffdorf002, wnk001]   
 
 /* PLATZIEREN DER BPLAN-INFORMATIONEN */    
 
