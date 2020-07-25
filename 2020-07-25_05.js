e44dc7683c74863aee77f2ad7cd860a12c91d1c5
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-07-25 18:11:58 +0200

    Update and rename 2020-07-25_04.js to 2020-07-25_05.js

--- 2020-07-25_05.js
+++ 2020-07-25_05.js
@@ -32,12 +32,12 @@ bremerhaven001 = ["bremerhaven001", 'Bebauungsplan Nr. 494 "Green Economy-Gebiet
 
 loxstedt001 = ["loxstedt001", "derzeit keine Stellungnahmen","","",""]
 
-schiffdorf002 = ["schiffdorf992", 'Wehdel: Bebauungsplan Nr. 96 "Moorteilsweg"', "wehdel-moorteilsweg", "Öffentlichkeit", "07/2020"]
+schiffdorf002 = ["schiffdorf002", 'Wehdel: Bebauungsplan Nr. 96 "Moorteilsweg"', "wehdel-moorteilsweg", "Öffentlichkeit", "07/2020"]
 schiffdorf001 = ["schiffdorf001", 'Spaden: Bebauungsplan Nr. 103 "Betreutes Wohnen am Schierholzweg"',"spaden-betreutes-wohnen-am-schierholzweg/","Öffentlichkeit","05/2020"]
 
 wnk001 = ["wnk001", 'Wremen: Bebauungsplan Nr. 32 "Wohnanlage Marschenhof"', "wremen-wohnanlage-marschenhof/", "frühzeitig", "05/2020"]
 
-bplaene = [geestland001, geestland002, geestland003, geestland004, bremerhaven001, loxstedt001, schiffdorf001, schiffdorf002, wnk001]
+bplaene = [geestland001, geestland002, geestland003, geestland004, bremerhaven001, loxstedt001, schiffdorf001, schiffdorf002, wnk001, wnk001]
 
 /* PLATZIEREN DER BPLAN-INFORMATIONEN */    
 
